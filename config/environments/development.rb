026c5fd5b6261b832eedaf938b0f71ccb08e9690
Steven Nguyen <stevennguyen@motorist.vn>
2026-09-15 20:28:01 +0700

    mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,6 +47,16 @@ Rails.application.configure do
   # Set localhost to be used by links generated in mailer templates.
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    user_name: ENV.fetch("MAILER_ADDRESS"),
+    password: ENV.fetch("MAILER_PASSWORD"),
+    authentication: "plain",
+    enable_starttls_auto: true
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
